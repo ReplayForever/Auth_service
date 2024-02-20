ad285f813bd7552652e40ad9d3ccaf599b42d630
Леонид Масловский <maslovskiy@metasharks.ru>
2024-02-20 19:09:39 +0300

    Создание ручек для сервиса

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 
+from api.v1 import auth, profile
 from core.config import settings
 from core.logger import LOGGING
 from db import redis
@@ -12,7 +13,6 @@ from db import redis
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    redis.redis = Redis(host=settings.redis.host, port=settings.redis.port)
     redis.redis = Redis(host=settings.redis.host, port=settings.redis.port)
 
     yield
@@ -29,6 +29,10 @@ app = FastAPI(
     version="1.0.0",
     lifespan=lifespan
 )
+
+
+app.include_router(auth.router, prefix='/api/v1', tags=['auth'])
+app.include_router(profile.router, prefix='/api/v1', tags=['profile'])
 
 
 if __name__ == '__main__':

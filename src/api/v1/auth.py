6e73eaad62bf40296d7d075c898acae943e5839f
German Chaley <chaleyg2@gmail.com>
2024-02-24 16:43:53 +0300

    Проба логина

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -1,8 +1,12 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Cookie
+from fastapi import APIRouter, Cookie, Depends, HTTPException, status, Request
+from sqlalchemy.orm import Session
 
+from db.postgres import get_session
+from models.schemas import User
 from models.users import UserLogin, UserSuccessLogin, UserCreate, UserSuccessRefreshToken
+from services.jwt import JWT
 
 router = APIRouter()
 
@@ -15,8 +19,27 @@ async def signup(user_create: UserCreate) -> None:
 
 @router.post('/login/',
              description="Аутентификация пользователя")
-async def login(user_auth: UserLogin) -> UserSuccessLogin:
-    return None
+async def login(user_auth: UserLogin,
+                db: Session = Depends(get_session),
+                jwt: JWT = Depends(),
+                request: Request = None) -> UserSuccessLogin:
+    user = await db.execute(User.query.filter(User.login == user_auth.login).first())
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Пользователь с таким логином не найден')
+
+    user = user.scalar()
+
+    if not user.check_password(user_auth.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Неверный пароль')
+
+    access_token = await jwt.create_access_token(user_id=str(user.id))
+    refresh_token = await jwt.create_refresh_token(user_id=str(user.id))
+
+    await jwt.set_access_token(token=access_token, user_id=str(user.id), role_id=user.role_id,
+                               expires_time=3600)
+    await jwt.set_refresh_token(token=refresh_token, user_agent=request.headers.get('User-Agent'))
+
+    return UserSuccessLogin(access_token=access_token, refresh_token=refresh_token)
 
 
 @router.post('/logout/',

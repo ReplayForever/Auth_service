b91f2007aa30ef40ef784fd84469e096150b9c4f
German Chaley <chaleyg2@gmail.com>
2024-03-01 19:15:24 +0300

    Рабочий профайл

--- src/services/profile.py
+++ src/services/profile.py
@@ -34,7 +34,7 @@ class ProfileHistoryService(AbstractService):
         self._db = db
         self._authorize = authorize
 
-    async def get_data(self, token, page, limit) -> Paginator:
+    async def get_data(self, page, limit) -> Paginator:
         await self._authorize.jwt_required()
 
         user_id = await self._authorize.get_jwt_subject()
@@ -55,49 +55,51 @@ class ProfileHistoryService(AbstractService):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='History not found')
 
         return Paginator(page=page, limit=limit, results=history_list)
-#
-#
-# class ProfileUpdateInfoService(PatchAbstractService):
-#     def __init__(self, db: AsyncSession, jwt: JWT):
-#         self._db = db
-#         self._jwt = jwt
-#
-#     async def patch(self, token, user_info: ChangeUserProfile) -> UserProfileResult:
-#         token_info = await self._jwt.get_access_token(token)
-#         user = await self._db.get(User, token_info['user_id'])
-#
-#         for attr, value in user_info.dict().items():
-#             if value:
-#                 setattr(user, attr, value)
-#
-#         user.modified_at = datetime.datetime.now()
-#
-#         await self._db.commit()
-#         await self._db.refresh(user)
-#         return UserProfileResult(**user.__dict__)
-#
-#
-# class UpdatePasswordService(PatchAbstractService):
-#     def __init__(self, db: AsyncSession, jwt: JWT):
-#         self._db = db
-#         self._jwt = jwt
-#
-#     async def patch(self, token, passwords: UserChangePassword) -> None:
-#         token_info = await self._jwt.get_access_token(token)
-#         user = await self._db.get(User, token_info['user_id'])
-#
-#         if not user.check_password(passwords.password):
-#             raise HTTPException(status_code=400, detail='Wrong password')
-#         elif not validate_password(passwords.new_password):
-#             raise HTTPException(status_code=400, detail='Password too simple')
-#
-#         user.password = generate_password_hash(passwords.new_password)
-#         user.modified_at = datetime.datetime.now()
-#
-#         await self._db.commit()
-#         await self._db.refresh(user)
-#
-#
+
+
+class ProfileUpdateInfoService(PatchAbstractService):
+    def __init__(self, db: AsyncSession, authorize: AuthJWT):
+        self._db = db
+        self._authorize = authorize
+
+    async def patch(self, user_info: ChangeUserProfile) -> UserProfileResult:
+        await self._authorize.jwt_required()
+
+        user_id = await self._authorize.get_jwt_subject()
+        user = await self._db.get(User, user_id)
+
+        for attr, value in user_info.dict().items():
+            if value:
+                setattr(user, attr, value)
+
+        user.modified_at = datetime.datetime.now()
+
+        await self._db.commit()
+        await self._db.refresh(user)
+        return UserProfileResult(**user.__dict__)
+
+
+class UpdatePasswordService(PatchAbstractService):
+    def __init__(self, db: AsyncSession, authorize: AuthJWT):
+        self._db = db
+        self._authorize = authorize
+
+    async def patch(self, passwords: UserChangePassword) -> None:
+        await self._authorize.jwt_required()
+
+        user_id = await self._authorize.get_jwt_subject()
+        user = await self._db.get(User, user_id)
+
+        if not user.check_password(passwords.password):
+            raise HTTPException(status_code=400, detail='Wrong password')
+        elif not validate_password(passwords.new_password):
+            raise HTTPException(status_code=400, detail='Password too simple')
+
+        user.password = generate_password_hash(passwords.new_password)
+        user.modified_at = datetime.datetime.now()
+
+        await self._db.commit()
+        await self._db.refresh(user)
 
 
 @lru_cache()
@@ -106,27 +108,27 @@ def get_profile_info_service(
         authorize: AuthJWT = Depends(),
 ) -> ProfileInfoService:
     return ProfileInfoService(db, authorize)
-#
-#
-# @lru_cache()
-# def get_profile_history_service(
-#         db: AsyncSession = Depends(get_session),
-#         jwt: JWT = Depends(get_jwt),
-# ) -> ProfileHistoryService:
-#     return ProfileHistoryService(db, jwt)
-#
-#
-# @lru_cache()
-# def patch_profile_info_service(
-#         db: AsyncSession = Depends(get_session),
-#         jwt: JWT = Depends(get_jwt),
-# ) -> ProfileUpdateInfoService:
-#     return ProfileUpdateInfoService(db, jwt)
-#
-#
-# @lru_cache()
-# def update_password_service(
-#         db: AsyncSession = Depends(get_session),
-#         jwt: JWT = Depends(get_jwt),
-# ) -> UpdatePasswordService:
-#     return UpdatePasswordService(db, jwt)
+
+
+@lru_cache()
+def get_profile_history_service(
+        db: AsyncSession = Depends(get_session),
+        authorize: AuthJWT = Depends(),
+) -> ProfileHistoryService:
+    return ProfileHistoryService(db, authorize)
+
+
+@lru_cache()
+def patch_profile_info_service(
+        db: AsyncSession = Depends(get_session),
+        authorize: AuthJWT = Depends(),
+) -> ProfileUpdateInfoService:
+    return ProfileUpdateInfoService(db, authorize)
+
+
+@lru_cache()
+def update_password_service(
+        db: AsyncSession = Depends(get_session),
+        authorize: AuthJWT = Depends(),
+) -> UpdatePasswordService:
+    return UpdatePasswordService(db, authorize)

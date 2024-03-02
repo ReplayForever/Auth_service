1319935657f51ff7d45a508e9f51b81833580444
SekretovKirill <sekir2011@gmail.com>
2024-03-02 20:28:50 +0300

    Добавлена проверка валидности токена

---
+++ src/services/common/access_check_common.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException
+from redis.exceptions import ResponseError
+
+
+class AccessCheckCommon:
+    _authorize = None
+    _redis_token = None
+
+    async def check_access(self):
+        await self._authorize.jwt_required()
+        raw_token = await self._authorize.get_raw_jwt()
+        access_jti = (raw_token)["jti"]
+        try:
+            if await self._redis_token.exists(access_jti):
+                raise HTTPException(status_code=403, detail="Access token is invalid or expired")
+        except ResponseError:
+            raise HTTPException(status_code=500, detail="Error connecting to Redis")
--- src/services/roles.py
+++ src/services/roles.py
@@ -2,6 +2,7 @@ import datetime
 from functools import lru_cache
 
 from async_fastapi_jwt_auth import AuthJWT
+from redis.asyncio import Redis
 from fastapi import Depends, HTTPException
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -10,18 +11,22 @@ from starlette import status
 from starlette.requests import Request
 
 from db.postgres import get_session
+from db.redis import get_redis
 from models.schemas import Role
 from models.roles import CreateRole, RoleChangePermission, RoleToRepresentation
 from services.abstract import AbstractService, PatchAbstractService, CreateAbstractService, DeleteAbstractService
 from services.common.roles_common import RolesCommon
+from services.common.access_check_common import AccessCheckCommon
 
 
-class RoleGetService(AbstractService, RolesCommon):
-    def __init__(self, db: AsyncSession, authorize: AuthJWT):
+class RoleGetService(AbstractService, RolesCommon, AccessCheckCommon):
+    def __init__(self, db: AsyncSession, authorize: AuthJWT, redis_token: Redis):
         self._db = db
         self._authorize = authorize
+        self._redis_token = redis_token
 
     async def get_data(self, request: Request) -> list[RoleToRepresentation]:
+        await self.check_access()
         await self.check_auth()
 
         roles = await self._db.execute(select(Role))
@@ -29,13 +34,15 @@ class RoleGetService(AbstractService, RolesCommon):
         return [RoleToRepresentation(**role[0].__dict__) for role in roles]
 
 
-class RoleCreateService(CreateAbstractService, RolesCommon):
-    def __init__(self, db: AsyncSession, authorize: AuthJWT):
+class RoleCreateService(CreateAbstractService, RolesCommon, AccessCheckCommon):
+    def __init__(self, db: AsyncSession, authorize: AuthJWT, redis_token: Redis):
         self._db = db
         self._authorize = authorize
+        self._redis_token = redis_token
 
     async def create(self, role: CreateRole, request: Request) -> RoleToRepresentation:
-        await self.check_auth()
+        await self.check_access()
+        await self.check_auth(request)
 
         new_role = Role(**role.dict())
         self._db.add(new_role)
@@ -48,12 +55,14 @@ class RoleCreateService(CreateAbstractService, RolesCommon):
         return RoleToRepresentation(**new_role.__dict__)
 
 
-class RoleDeleteService(DeleteAbstractService, RolesCommon):
-    def __init__(self, db: AsyncSession, authorize: AuthJWT):
+class RoleDeleteService(DeleteAbstractService, RolesCommon, AccessCheckCommon):
+    def __init__(self, db: AsyncSession, authorize: AuthJWT, redis_token: Redis):
         self._db = db
         self._authorize = authorize
+        self._redis_token = redis_token
 
     async def delete(self, role_id: int, request: Request) -> None:
+        await self.check_access()
         await self.check_auth()
 
         role = await self._db.get(Role, role_id)
@@ -65,12 +74,14 @@ class RoleDeleteService(DeleteAbstractService, RolesCommon):
         await self._db.commit()
 
 
-class RoleUpdateService(PatchAbstractService, RolesCommon):
-    def __init__(self, db: AsyncSession, authorize: AuthJWT):
+class RoleUpdateService(PatchAbstractService, RolesCommon, AccessCheckCommon):
+    def __init__(self, db: AsyncSession, authorize: AuthJWT, redis_token: Redis):
         self._db = db
         self._authorize = authorize
+        self._redis_token = redis_token
 
     async def patch(self, role_id: int, role_update: RoleChangePermission, request: Request) -> RoleToRepresentation:
+        await self.check_access()
         await self.check_auth()
 
         role = await self._db.get(Role, role_id)
@@ -91,29 +102,33 @@ class RoleUpdateService(PatchAbstractService, RolesCommon):
 def get_role_create_service(
         db: AsyncSession = Depends(get_session),
         authorize: AuthJWT = Depends(),
+        redis_token: Redis = Depends(get_redis),
 ) -> RoleCreateService:
-    return RoleCreateService(db, authorize)
+    return RoleCreateService(db, authorize, redis_token)
 
 
 @lru_cache()
 def get_role_get_service(
         db: AsyncSession = Depends(get_session),
         authorize: AuthJWT = Depends(),
+        redis_token: Redis = Depends(get_redis),
 ) -> RoleGetService:
-    return RoleGetService(db, authorize)
+    return RoleGetService(db, authorize, redis_token)
 
 
 @lru_cache()
 def get_role_delete_service(
         db: AsyncSession = Depends(get_session),
         authorize: AuthJWT = Depends(),
+        redis_token: Redis = Depends(get_redis),
 ) -> RoleDeleteService:
-    return RoleDeleteService(db, authorize)
+    return RoleDeleteService(db, authorize, redis_token)
 
 
 @lru_cache()
 def get_role_update_service(
         db: AsyncSession = Depends(get_session),
         authorize: AuthJWT = Depends(),
+        redis_token: Redis = Depends(get_redis),
 ) -> RoleUpdateService:
-    return RoleUpdateService(db, authorize)
+    return RoleUpdateService(db, authorize, redis_token)

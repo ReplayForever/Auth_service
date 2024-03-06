c389805c592093d5d9c69dde069a0696254289ab
Леонид Масловский <maslovskiy@metasharks.ru>
2024-03-06 23:21:23 +0300

    Обновлены фикстуры Исправлены тесты для ролей

--- tests/functional/src/test_user_profile.py
+++ tests/functional/src/test_user_profile.py
@@ -5,8 +5,8 @@ from functional.settings import settings
 
 
 @pytest.mark.parametrize('admin, login, password, expected_status', [
-    (True, 'testlogin', 'testpassword1!S', status.HTTP_201_CREATED),
-    (True, 'testlogin', 't', status.HTTP_400_BAD_REQUEST),
+    (True, 'testloginTrue', 'testpassword1!S', status.HTTP_201_CREATED),
+    (True, 'testloginTrue', 't', status.HTTP_400_BAD_REQUEST),
     (True, 'testlogin123', 'testpassword1!S', status.HTTP_400_BAD_REQUEST),
 ])
 @pytest.mark.asyncio
@@ -24,34 +24,26 @@ async def test_user_login(aiohttp_client, create_user, admin, login, password, e
 
 
 @pytest.mark.parametrize('admin, login, password, expected_status', [
-    (True, 'testlogin', 'testpassword1!S', status.HTTP_201_CREATED),
+    (True, 'testloginTrue', 'testpassword1!S', status.HTTP_201_CREATED),
+    (False, 'testloginFalse', 'testpassword1!S', status.HTTP_201_CREATED),
 ])
 @pytest.mark.asyncio
-async def test_get_user_profile(aiohttp_client, create_user, admin, login, password, expected_status):
-    user_data = {
-        'login': login,
-        'password': password
-    }
-
-    url = f'{settings.fastapi.url()}/login/'
-    headers = {}
-    async with aiohttp_client.post(url, json=user_data, headers=headers) as response:
-        if 'Content-Type' in response.headers and 'application/json' in response.headers['Content-Type']:
-            access_token_cookie = response.cookies['access_token_cookie'].value
-            cookies = {'access_token_cookie': access_token_cookie}
+async def test_get_user_profile(aiohttp_client, create_user, login_user, admin, login, password, expected_status):
+    tokens = await login_user(login, password)
+    cookies = {'access_token_cookie': tokens['access_token_cookie'].value}
 
     url = f'{settings.fastapi.url()}/profile/'
-    async with aiohttp_client.get(url, cookies=cookies, headers=headers) as response:
+    async with aiohttp_client.get(url, cookies=cookies) as response:
         body = None
         response_status = response.status
         if 'Content-Type' in response.headers and 'application/json' in response.headers['Content-Type']:
             body = await response.json()
     user_info = {
-        'username': 'testname',
+        'username': 'testname%s' % str(admin),
         'login': login,
         'first_name': 'string',
         'last_name': 'string',
-        'email': 'test@example.ru',
+        'email': 'test@example.ru%s' % str(admin),
         'birth_day': '2024-02-29',
         'picture': 'string'}
     body.pop('role_id')
@@ -60,21 +52,13 @@ async def test_get_user_profile(aiohttp_client, create_user, admin, login, passw
 
 
 @pytest.mark.parametrize('admin, login, password, expected_status', [
-    (True, 'testlogin', 'testpassword1!S', status.HTTP_201_CREATED),
+    (True, 'testloginTrue', 'testpassword1!S', status.HTTP_201_CREATED),
+    (False, 'testloginFalse', 'testpassword1!S', status.HTTP_201_CREATED),
 ])
 @pytest.mark.asyncio
-async def test_patch_user_profile(aiohttp_client, create_user, admin, login, password, expected_status):
-    user_data = {
-        'login': login,
-        'password': password
-    }
-
-    url = f'{settings.fastapi.url()}/login/'
-    headers = {}
-    async with aiohttp_client.post(url, json=user_data, headers=headers) as response:
-        if 'Content-Type' in response.headers and 'application/json' in response.headers['Content-Type']:
-            access_token_cookie = response.cookies['access_token_cookie'].value
-            cookies = {'access_token_cookie': access_token_cookie}
+async def test_patch_user_profile(aiohttp_client, create_user, login_user, admin, login, password, expected_status):
+    tokens = await login_user(login, password)
+    cookies = {'access_token_cookie': tokens['access_token_cookie'].value}
 
     url = f'{settings.fastapi.url()}/profile/'
     new_user_data = {
@@ -84,13 +68,13 @@ async def test_patch_user_profile(aiohttp_client, create_user, admin, login, pas
         "birth_day": "2020-01-01",
         "picture": "path",
     }
-    async with aiohttp_client.patch(url, cookies=cookies, headers=headers, json=new_user_data) as response:
+    async with aiohttp_client.patch(url, cookies=cookies, json=new_user_data) as response:
         body = None
         response_status = response.status
         if 'Content-Type' in response.headers and 'application/json' in response.headers['Content-Type']:
             body = await response.json()
     user_info = {
-        'username': 'testname',
+        'username': 'testname%s' % str(admin),
         'login': login,
         "first_name": "123",
         "last_name": "123",
@@ -105,24 +89,16 @@ async def test_patch_user_profile(aiohttp_client, create_user, admin, login, pas
 
 
 @pytest.mark.parametrize('admin, login, password, expected_status', [
-    (True, 'testlogin', 'testpassword1!S', status.HTTP_201_CREATED),
+    (True, 'testloginTrue', 'testpassword1!S', status.HTTP_201_CREATED),
+    (False, 'testloginFalse', 'testpassword1!S', status.HTTP_201_CREATED),
 ])
 @pytest.mark.asyncio
-async def test_user_history(aiohttp_client, create_user, admin, login, password, expected_status):
-    user_data = {
-        'login': login,
-        'password': password
-    }
-
-    url = f'{settings.fastapi.url()}/login/'
-    headers = {}
-    async with aiohttp_client.post(url, json=user_data, headers=headers) as response:
-        if 'Content-Type' in response.headers and 'application/json' in response.headers['Content-Type']:
-            access_token_cookie = response.cookies['access_token_cookie'].value
-            cookies = {'access_token_cookie': access_token_cookie}
+async def test_user_history(aiohttp_client, create_user, login_user, admin, login, password, expected_status):
+    tokens = await login_user(login, password)
+    cookies = {'access_token_cookie': tokens['access_token_cookie'].value}
 
     url = f'{settings.fastapi.url()}/profile/history?page=1&limit=10'
-    async with aiohttp_client.get(url, cookies=cookies, headers=headers) as response:
+    async with aiohttp_client.get(url, cookies=cookies) as response:
         body = None
         response_status = response.status
         if 'Content-Type' in response.headers and 'application/json' in response.headers['Content-Type']:

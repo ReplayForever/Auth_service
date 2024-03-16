04eafaae292da507855588696242b37d4c66af9f
German Chaley <chaleyg2@gmail.com>
2024-03-16 21:03:43 +0300

    fix endpoint

--- src/api/v1/oauth.py
+++ src/api/v1/oauth.py
@@ -9,22 +9,29 @@ from services.oauth import (YandexAuthServiceCallback, YandexAuthServiceLogin,
 
 router = APIRouter()
 
+YANDEX = 'yandex'
 
-@router.get('/login/yandex',
-            description='Логин через Яндекс',
+
+@router.get('/login/social_network/{provider}',
+            description='Логин через соц.сеть',
             status_code=HTTPStatus.OK,
             tags=['oauth'])
-async def yandex_login(yandex_user: YandexAuthServiceLogin = Depends(get_yandex_login_service)):
-    await yandex_user.get_data()
+async def social_network_login(
+        provider: str,
+        yandex_user: YandexAuthServiceLogin = Depends(get_yandex_login_service)):
+    if provider.lower() == YANDEX:
+        await yandex_user.get_data()
+    else:
+        raise HTTPException(HTTPStatus.BAD_REQUEST, detail='Invalid provider')
 
 
-@router.get('/login/yandex/callback',
+@router.get('/login/callback',
             description='Функционал работы с пользователем',
             status_code=HTTPStatus.OK,
             tags=['oauth'])
-async def yandex_callback(code: str,
-                          user_agent: str = Header(""),
-                          user_data: YandexAuthServiceCallback = Depends(get_yandex_callback_service)):
+async def social_network_callback(code: str,
+                                  user_agent: str = Header(""),
+                                  user_data: YandexAuthServiceCallback = Depends(get_yandex_callback_service)):
     result = await user_data.get_data(code, user_agent)
     if not result:
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='Login error')
